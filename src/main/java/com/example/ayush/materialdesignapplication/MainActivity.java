5d80d8f1ee2b301bb341a35a94a484d6d45cd457
ash2712 <spellguru@gmail.com>
2017-09-29 21:49:16 +0530

    Add files via upload

---
+++ src/main/java/com/example/ayush/materialdesignapplication/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.ayush.materialdesignapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    public ArrayList<String> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        list.add("Alpha");
+        list.add("Beta");
+        list.add("Cupcake");
+        list.add("Donut");
+        list.add("Eclair");
+        list.add("Froyo");
+        list.add("Gingerbread");
+        list.add("Honeycomb");
+        list.add("Ice Cream Sandwich");
+        list.add("Jelly Bean");
+        list.add("Kit kat");
+        list.add("Lollypop");
+        list.add("Marshmellow");
+        list.add("Nougat");
+        list.add("Oreo");
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        adapter adapter = new adapter(MainActivity.this,list);
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+}
+
+
+
+
+
+
---
+++ src/main/java/com/example/ayush/materialdesignapplication/adapter.java
@@ -0,0 +1,64 @@
+package com.example.ayush.materialdesignapplication;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by admin on 9/29/2017.
+ */
+
+public class adapter extends RecyclerView.Adapter<adapter.ViewHolder>{
+    //MyAdapter class extending the recyclerview.adapter
+    private ArrayList<String> list;
+    private Context context;
+    //declaring list and context
+    //parametrized constructor with context and arraylist as parameters
+    public adapter(Context context,ArrayList<String> list) {
+        super();
+        this.list = list;
+        this.context = context;
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //inflating the row layout
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row,parent,false);
+        //setting this view to viewholder
+        ViewHolder viewholder = new ViewHolder(view);
+        //returning view holder
+        return viewholder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        //setting textview
+        holder.textview.setText(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        //returning size of list
+        return list.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public CardView cardview;
+        public TextView textview;
+        public ViewHolder(View itemView) {
+
+            super(itemView);
+            //associating cardlayout and Textview from row.XML
+            textview = (TextView)itemView.findViewById(R.id.textview );
+            cardview = (CardView)itemView.findViewById(R.id.cardView);
+        }
+    }
+}

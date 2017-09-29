f10b787d9a5b881351450946caed65136d3326f1
ash2712 <spellguru@gmail.com>
2017-09-29 21:52:57 +0530

    Update adapter.java

--- src/main/java/com/example/ayush/materialdesignapplication/adapter.java
+++ src/main/java/com/example/ayush/materialdesignapplication/adapter.java
@@ -11,11 +11,11 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 /**
- * Created by admin on 9/29/2017.
+ * Created by ayush on 9/29/2017.
  */
 
 public class adapter extends RecyclerView.Adapter<adapter.ViewHolder>{
-    //MyAdapter class extending the recyclerview.adapter
+    //adapter class extending the recyclerview.adapter
     private ArrayList<String> list;
     private Context context;
     //declaring list and context
@@ -31,7 +31,6 @@ public class adapter extends RecyclerView.Adapter<adapter.ViewHolder>{
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //inflating the row layout
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row,parent,false);
-        //setting this view to viewholder
         ViewHolder viewholder = new ViewHolder(view);
         //returning view holder
         return viewholder;
